fd5779dfe209d5e3131c78e0723f092dddcaf7ec
Matheus Marins <matheus.marins2011@hotmail.com>
2023-09-06 17:34:03 -0300

    refactor: refactor searchInfoUser  and listCommands

--- src/discord/commands/listCommands/listCommands.ts
+++ src/discord/commands/listCommands/listCommands.ts
@@ -7,19 +7,27 @@ export default new Command({
   dmPermission: false,
   type: ApplicationCommandType.ChatInput,
   async run ({ interaction }) {
-    const listCommands = new EmbedBuilder()
-      .setTitle('Sr. Pierre, o Mordomo ao seu dispor. Merci\n\nLista de comandos')
-      .addFields(
-        { name: 'investigar', value: 'Eu me disponho a trazer um relatório minucioso, abrangendo todas as informações que encontrei.' },
-        { name: 'investigarjogador', value: 'Eu me disponho a trazer um relatório minucioso, abrangendo todas as informações que encontrei sobre o jogador.' }
-      )
-      .setFooter({
-        text: 'Permita-me apresentar uma lista de comandos Apps disponíveis, prontamente à vossa disposição.',
-        iconURL: interaction.client.user.avatarURL() || undefined
-      })
-      .setTimestamp()
-      .setThumbnail(interaction.client.user.avatarURL())
-      .setColor('White')
-    interaction.reply({ embeds: [listCommands] })
+    try {
+      const listCommands = new EmbedBuilder()
+        .setTitle('Sr. Pierre, o Mordomo ao seu dispor. Merci\n\nLista de comandos')
+        .addFields(
+          { name: 'investigar', value: 'Eu me disponho a trazer um relatório minucioso, abrangendo todas as informações que encontrei.' },
+          { name: 'investigarjogador', value: 'Eu me disponho a trazer um relatório minucioso, abrangendo todas as informações que encontrei sobre o jogador.' }
+        )
+        .setFooter({
+          text: 'Permita-me apresentar uma lista de comandos Apps disponíveis, prontamente à vossa disposição.',
+          iconURL: interaction.client.user.avatarURL() || undefined
+        })
+        .setTimestamp()
+        .setThumbnail(interaction.client.user.avatarURL())
+        .setColor('White')
+      interaction.reply({ embeds: [listCommands] })
+    } catch (error) {
+      console.error('Erro ao processar o comando:', error)
+      const errorResponse = new EmbedBuilder()
+        .setDescription('Desculpe, ocorreu um erro ao processar o comando. Por favor, tente novamente mais tarde.')
+        .setColor('Red')
+      interaction.editReply({ embeds: [errorResponse] })
+    }
   }
 })
